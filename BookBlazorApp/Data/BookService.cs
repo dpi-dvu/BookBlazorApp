810b184eed5083061704200dea9894c5fdb459bb
Don Vu <Don.Vu@fdacs.gov>
2022-11-22 13:50:26 -0500

    Add project files.

---
+++ BookBlazorApp/Data/BookService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookBlazorApp.Data
+{
+    public class BookService
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BookService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<Book>> GetAllBooksAsync()
+        {
+            return await _appDbContext.Books.ToListAsync();
+        }
+
+        public async Task<bool> InsertBookAsync(Book book)
+        {
+            await _appDbContext.Books.AddAsync(book);
+            await _appDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Book?> GetBookAsync(int Id)
+        {
+            return await _appDbContext.Books.FirstOrDefaultAsync(b => b.Id.Equals(Id));
+        }
+
+        public async Task<bool> UpdateBookAsync(Book book)
+        {
+            _appDbContext.Books.Update(book);
+            await _appDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteBookAsync(Book book)
+        {
+            _appDbContext.Books.Remove(book);
+            await _appDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ BookBlazorApp/Program.cs
@@ -0,0 +1,50 @@
+using BookBlazorApp.Data;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<BookService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options
+    .UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\vud\\Documents\\Projects\\BookBlazorApp\\BookBlazorApp\\Database\\BookDatabase.mdf;Integrated Security=True"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
