5fcccd71b414e82e66f6dac5358b273e97224850
Don Vu <Don.Vu@fdacs.gov>
2022-11-28 15:32:44 -0500

    Added Book constructor, error handling for edit book at unknown Id

--- BookBlazorApp/Data/Book.cs
+++ BookBlazorApp/Data/Book.cs
@@ -1,10 +1,20 @@
-﻿namespace BookBlazorApp.Data
+﻿
+namespace BookBlazorApp.Data
 {
     public class Book
     {
+        public Book(string title, string author, string genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
+
+        public Book() : this("Untitled", "Unknown","Misc") { }
+
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Author { get; set; }
-        public string? Genre { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
     }
 }
